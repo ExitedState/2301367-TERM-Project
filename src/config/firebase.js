d077561ca83175f4b6601ecbd56b97693d90b92f
Phaiboon A <phaiboon760@gmail.com>
2023-11-11 10:00:41 +0700

    update env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,15 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  // Your Firebase config here
-  apiKey: "AIzaSyDYdE1xz3i6QDiige2FnTqmmj4PLHWljG8",
-  authDomain: "setermproject-5ea97.firebaseapp.com",
-  databaseURL: "https://setermproject-5ea97-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "setermproject-5ea97",
-  storageBucket: "setermproject-5ea97.appspot.com",
-  messagingSenderId: "533125317536",
-  appId: "1:533125317536:web:aff9cf62a7fbcb855f5ac5",
-  measurementId: "G-HFDFGLWCCN"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
