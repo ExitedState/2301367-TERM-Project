678f464f533ced69a3c9a735f8d39de163765899
Phaiboon A <67526393+ExitedState@users.noreply.github.com>
2023-11-13 09:10:37 +0700

    Refactor MainPage component layout (#29)

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -25,55 +25,60 @@ export default function MainPage() {
 
     return (
         <Container className="min-vh-100 d-flex flex-column justify-content-between">
-            <Row className="mt-5">
-                <Col xs={12} md={4} lg={3} className="text-center text-md-left">
-                    <img src={logo} alt="Logo" className="img-fluid" style={{ maxWidth: '120px' }} />
-                    <p class="text-Color">· AccessiBus ·</p>
-                </Col>
-                <Col xs={12} md={8} lg={9} className="d-flex align-items-center justify-content-md-end">
-                    {!currentUser ? (
-                        <>
-                            <Link to="/login">
-                                <Button variant="outline-success" className="mx-2">Login</Button>
-                            </Link>
-                            <Link to="/signup">
-                                <Button variant="outline-success" className="mx-2">Register</Button>
-                            </Link>
-                        </>
-                    ) : (
-                        <>
-                            <Link to="/findRoute">
-                                <Button variant="outline-success" className="mx-2">FindBus</Button>
-                            </Link>
-                            <Link to="/favoRoute">
-                                <Button variant="outline-success" className="mx-2">Favorite</Button>
-                            </Link>
-                            <Link to="/update-profile">
-                                <Button variant="outline-success" className="mx-2">Profile</Button>
-                            </Link>
-                            <Button variant="outline-danger" className="mx-2" onClick={handleLogout}>Logout</Button>
-                        </>
-                    )}
-                </Col>
-            </Row>
+            <div>
+                <Row className="mt-5">
+                    <Col xs={12} md={4} lg={3} className="text-center text-md-left">
+                        <img src={logo} alt="Logo" className="img-fluid" style={{ maxWidth: '120px' }} />
+                        <p class="text-Color">· AccessiBus ·</p>
+                    </Col>
+                    <Col xs={12} md={8} lg={9} className="d-flex align-items-center justify-content-md-end">
+                        {!currentUser ? (
+                            <>
+                                <Link to="/login">
+                                    <Button variant="outline-success" className="mx-2">Login</Button>
+                                </Link>
+                                <Link to="/signup">
+                                    <Button variant="outline-success" className="mx-2">Register</Button>
+                                </Link>
+                            </>
+                        ) : (
+                            <>
+                                <Link to="/findRoute">
+                                    <Button variant="outline-success" className="mx-2">FindBus</Button>
+                                </Link>
+                                <Link to="/favoRoute">
+                                    <Button variant="outline-success" className="mx-2">Favorite</Button>
+                                </Link>
+                                <Link to="/update-profile">
+                                    <Button variant="outline-success" className="mx-2">Profile</Button>
+                                </Link>
+                                <Button variant="outline-danger" className="mx-2" onClick={handleLogout}>Logout</Button>
+                            </>
+                        )}
+                    </Col>
+                </Row>
+            </div>
 
-            {/* Toast Container for logout success message */}
-            <ToastContainer className="p-3" position="top-end">
-                <Toast onClose={() => setShowToast(false)} show={showToast} delay={3000} autohide>
-                    <Toast.Header>
-                        <strong className="me-auto">Logout Successful</strong>
-                    </Toast.Header>
-                    <Toast.Body>You have been logged out.</Toast.Body>
-                </Toast>
-            </ToastContainer>
-
-            <Row className="my-4 text-center">
-                <Col>
-                    <h1 className="display-4 text-light">Welcome to AccessiBus</h1>
-                    <img src={bus} alt="bus" className="my-3 img-fluid" style={{ maxWidth: '200px' }} />
-                    <h2 className="text-dark">---Ensuring accessible bus travel for everyone---</h2>
-                </Col>
-            </Row>
+            <div className="my-auto">
+                <Row className="my-4 text-center">
+                    <Col>
+                        <h1 className="display-4 text-light">Welcome to AccessiBus</h1>
+                        <img src={bus} alt="bus" className="my-3 img-fluid" style={{ maxWidth: '200px' }} />
+                        <h2 className="text-dark">---Ensuring accessible bus travel for everyone---</h2>
+                    </Col>
+                </Row>
+            </div>
+            <div>
+                {/* Toast Container for logout success message */}
+                <ToastContainer className="p-3" position="top-end">
+                    <Toast onClose={() => setShowToast(false)} show={showToast} delay={3000} autohide>
+                        <Toast.Header>
+                            <strong className="me-auto">Logout Successful</strong>
+                        </Toast.Header>
+                        <Toast.Body>You have been logged out.</Toast.Body>
+                    </Toast>
+                </ToastContainer>
+            </div>
 
             {/* ... other components ... */}
         </Container>
